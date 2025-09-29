893c24d08d0a7c5291ca13014320ce7ea424e501
Tergene Focus <tergenefocus@gmail.com>
2025-09-29 15:42:31 -0400

    Delete components directory

--- components/MessageBubble.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function MessageBubble({ text, isOwn }) {
-  const base = "max-w-[80%] p-3 rounded-2xl mb-2 inline-block break-words";
-  const style = isOwn
-    ? `${base} bg-blue-200 self-end rounded-br-none`
-    : `${base} bg-gray-200 self-start rounded-bl-none`;
-  return <div className={style}>{text}</div>;
-}
